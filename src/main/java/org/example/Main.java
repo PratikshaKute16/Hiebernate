32acaf30786d7c948a53248c8971c48bd122165f
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-02 18:29:13 +0530

    Initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import com.mysql.cj.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args)
     {
 
         System.out.println("Hello world!");
+        SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+
     }
 }
