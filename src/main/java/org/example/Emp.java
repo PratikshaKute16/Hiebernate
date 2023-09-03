2e4449923281eb74bcb2a5f9bf7fb6dcdc4e5fab
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-03 19:42:23 +0530

    Initial

---
+++ src/main/java/org/example/Emp.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "emp")
+
+public class Emp
+{
+    @Id
+    private int id;
+    private String name;
+    private  String city;
+
+    public Emp() {
+    }
+
+    public Emp(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+":"+this.name+":"+this.city;
+    }
+}
