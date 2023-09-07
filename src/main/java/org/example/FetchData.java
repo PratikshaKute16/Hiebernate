c4ea0ac11a8d2d52877cc5ca81ca50c64cd9d32f
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-07 10:59:37 +0530

    Initial

--- src/main/java/org/example/FetchData.java
+++ src/main/java/org/example/FetchData.java
@@ -9,7 +9,7 @@ import java.util.ConcurrentModificationException;
 public class FetchData
 {
     public static void main(String[]  args) {
-        Configuration cfg=new Configuration();
+        Configuration  cfg=new Configuration();
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory =cfg.buildSessionFactory();
 
