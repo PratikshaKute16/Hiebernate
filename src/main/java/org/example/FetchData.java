48c1ab70f136a834edd6f97cf0adb7332ee8bca5
Pratiksha Kute <pratikute1603@gmail.com>
2023-09-07 09:03:54 +0530

    Initial

---
+++ src/main/java/org/example/FetchData.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ConcurrentModificationException;
+
+public class FetchData
+{
+    public static void main(String[] args) {
+        Configuration cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory =cfg.buildSessionFactory();
+
+        Session  session=factory.openSession();
+        Address address=(Address)session.get(Address.class,1);
+
+       Emp emp=(Emp)session.load(Emp.class,11);
+        System.out.println(emp);
+     session.close();
+     factory.close();
+    }
+}
